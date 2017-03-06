2804a5da3d0ff14e66f60b9d988e833d5cd40546
Madministrator <766182@ehs-s-232-14.district196.org>
2017-03-06 10:39:38 -0600

    Start of branch Commit
    Just added the view to storyboard, also attached the view controller.
    Literally nothing else has been added.

---
+++ Band-Audition-App/concertWindsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  concertWindsViewController.swift
+//  Band-Audition-App
+//
+//  Created by VANDER HOEVEN, EVAN on 3/6/17.
+//  Copyright © 2017 Copy Pasta Coding. All rights reserved.
+//
+
+import UIKit
+
+class concertWindsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
