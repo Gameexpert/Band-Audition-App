dd9b8825daaeee4f0dd40f5256221b7881d1956b
Madministrator <766182@ehs-s-232-14.district196.org>
2017-03-07 11:07:29 -0600

    mid-work save 03/07/2017
    fully created GUI from the storyboard perspective. Constraints may need
    to be added for the UISwitch and it’s associated label. Move onto
    adding the UISegmented control.

--- Band-Audition-App/concertWindsViewController.swift
+++ Band-Audition-App/concertWindsViewController.swift
@@ -19,61 +19,77 @@ class concertWindsViewController: UIViewController
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     
-    @IBOutlet weak var upperLeftLabel: UILabel!
+    @IBOutlet weak var upperLeftLabel: UIButton!
     @IBOutlet weak var upperLeftData: UIButton!
-    
-    @IBOutlet weak var upperRightLabel: UILabel!
+
+    @IBOutlet weak var upperRightLabel: UIButton!
     @IBOutlet weak var upperRightData: UIButton!
     
-    @IBOutlet weak var middleLeftLabel: UILabel!
+    @IBOutlet weak var middleLeftLabel: UIButton!
     @IBOutlet weak var middleLeftData: UIButton!
     
-    @IBOutlet weak var middleRightLabel: UILabel!
+    @IBOutlet weak var middleRightLabel: UIButton!
     @IBOutlet weak var middleRightData: UIButton!
     
-    @IBOutlet weak var lowerLeftLabel: UILabel!
+    @IBOutlet weak var lowerLeftLabel: UIButton!
     @IBOutlet weak var lowerLeftData: UIButton!
     
-    @IBOutlet weak var lowerRightLabel: UILabel!
+    @IBOutlet weak var lowerRightLabel: UIButton!
     @IBOutlet weak var lowerRightData: UIButton!
     
-    
+    @IBOutlet weak var upperLeftStack: UIStackView!
+    @IBOutlet weak var upperRightStack: UIStackView!
+    @IBOutlet weak var middleLeftStack: UIStackView!
+    @IBOutlet weak var middleRightStack: UIStackView!
+    @IBOutlet weak var lowerLeftStack: UIStackView!
+    @IBOutlet weak var lowerRightStack: UIStackView!
+    @IBOutlet weak var mainStackView: UIStackView!
+
     //commentsView related objects
     @IBOutlet weak var commentsView: UITextView!
     var keyboardHeight: Int = 0
     var textViewCleared: Bool = false
+    
+    //See setUpDataControl to see it's transformations
+    var dataControl: UISegmentedControl = UISegmentedControl(items: ["Knowledge & Performance", "Etude 1", "Etude 2", "Sight Read"])
+    
+    //Structure to store data until we save the data
+    struct auditionProperty
+    {
+        static var first_name: String = ""
+        static var last_name: String = ""
+        static var instrument: String = ""
+        static var category: String = "" //Either "Varsity" or "Freshmen"
+        static var comments: String = ""
+        static var memorized: Bool = false
+        
+        static var scale1: Double = 0
+        static var scale2: Double = 0
+        static var scale3: Double = 0
+        static var scale4: Double = 0
+        static var scale5: Double = 0
+        
+        static var chromatic_scale: Double = 0
+        static var etude1_pitch: Double = 0
+        static var etude1_rhythm: Double = 0
+        static var etude1_articulation: Double = 0
+        static var etude1_dynamics: Double = 0
+        static var etude1_tone: Double = 0
+        static var etude1_style: Double = 0
+        
+        static var etude2_pitch: Double = 0
+        static var etude2_rhythm: Double = 0
+        static var etude2_articulation: Double = 0
+        static var etude2_dynamics: Double = 0
+        static var etude2_tone: Double = 0
+        static var etude2_style: Double = 0
+        
+        var read_pitch: Double = 0
+        var read_rhythm: Double = 0
+        
+        var finalScore: Double = 0
+    }
 
-    /*
-     //Outlets for everything in the gui, I'll add names as they are used later.
-     @IBOutlet weak var instrumentNameLabel: UILabel!
-     @IBOutlet weak var firstNameBox: UITextField!
-     @IBOutlet weak var lastNameBox: UITextField!
-     @IBOutlet weak var finalScoreLabel: UILabel!
-     @IBOutlet weak var saveButton: UIButton!
-     @IBOutlet weak var backButton: UIButton!
-     
-     @IBOutlet weak var mainStackView: UIStackView!
-     @IBOutlet weak var upperLeftStack: UIStackView!
-     @IBOutlet weak var upperRightStack: UIStackView!
-     @IBOutlet weak var lowerLeftStack: UIStackView!
-     @IBOutlet weak var lowerRightStack: UIStackView!
-     @IBOutlet weak var middleStack: UIStackView!
-     
-     @IBOutlet weak var upperLeftLabel: UIButton!
-     @IBOutlet weak var upperLeftData: UIButton!
-     @IBOutlet weak var upperRightLabel: UIButton!
-     @IBOutlet weak var upperRightData: UIButton!
-     @IBOutlet weak var lowerLeftLabel: UIButton!
-     @IBOutlet weak var lowerLeftData: UIButton!
-     @IBOutlet weak var lowerRightLabel: UIButton!
-     @IBOutlet weak var lowerRightData: UIButton!
-     @IBOutlet weak var middleLabel: UIButton!
-     @IBOutlet weak var middleData: UIButton!
-     //commentsView related objects
-     @IBOutlet weak var commentsView: UITextView!
-     var keyboardHeight: Int = 0
-     var textViewCleared: Bool = false
-     */
     override func viewDidLoad()
     {
         super.viewDidLoad()
