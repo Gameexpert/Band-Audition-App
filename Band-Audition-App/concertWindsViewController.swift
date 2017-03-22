54519f1328ba3a6b649a10ac6232e35da33ce7bb
Madministrator <766182@ehs-s-232-14.district196.org>
2017-03-22 10:27:06 -0500

    Pre-Merge Commit
    Everything is ready to go for the merge to master branch. Just saving
    for redundancy’s sake.

--- Band-Audition-App/concertWindsViewController.swift
+++ Band-Audition-App/concertWindsViewController.swift
@@ -394,6 +394,20 @@ class concertWindsViewController: UIViewController, UITextViewDelegate, UIPopove
         
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        let itemName: String = textField.restorationIdentifier!
+        switch itemName
+        {
+        case "firstNameBox":
+            auditionProperty.first_name = textField.text!
+        case "lastNameBox":
+            auditionProperty.last_name = textField.text!
+        default:
+            Swift.print("Error: TextField End Editing Default Called")
+        }
+    }
+    
     func segmentedControlValueChanged(segment: UISegmentedControl)
     {
         let controlIndex: Int = self.dataControl.selectedSegmentIndex
