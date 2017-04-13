5a71534987fa9e9692e99a306a127216e1b24755
Justin Eccles <735979@ehs-s-232-13.district196.org>
2017-04-13 11:18:35 -0500

    Created the UI for the new Entry View Controller

---
+++ Band-Audition-App/MainTabsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabsViewController.swift
+//  Band-Audition-App
+//
+//  Created by ECCLES, JUSTIN on 4/13/17.
+//  Copyright © 2017 Copy Pasta Coding. All rights reserved.
+//
+
+import UIKit
+
+class MainTabsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
