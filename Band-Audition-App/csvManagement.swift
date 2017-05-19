3545b0e4fa1040d11796528718beca2ffa6317b0
Madministrator <766182@ehs-s-232-14.district196.org>
2017-05-19 10:44:21 -0500

    fixed no-header CSV issue
    switched x==0 argument with a boolean variable argument called
    “firstInstance”

--- Band-Audition-App/csvManagement.swift
+++ Band-Audition-App/csvManagement.swift
@@ -5,7 +5,6 @@
 //  Created by ECCLES, JUSTIN on 5/3/17.
 //  Copyright © 2017 Copy Pasta Coding. All rights reserved.
 //
-
 import UIKit
 import MessageUI
 
@@ -15,6 +14,8 @@ class csvManagement: NSObject
     class func createCSV() -> Void
     {
         var csvText = ""
+        
+        var firstInstance: Bool = true
         
         var auditionsToConvert: [audition] = []
         switch arrayIdentifier
@@ -26,38 +27,44 @@ class csvManagement: NSObject
                 {
                     auditionsToConvert.append(varsityAuditions[x][i])
                 }
-                
-                for x in 0..<auditionsToConvert.count
+            }
+            for x in 0..<auditionsToConvert.count
+            {
+                if let rhythm = auditionsToConvert[x] as? concertWinds
                 {
-                    if let rhythm = auditionsToConvert[x] as? concertWinds
+                    if firstInstance
                     {
-                        if x == 0
-                        {
-                            csvText = "Varsity Concert Winds\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1, Scale 2, Scale 3, Scale 4, Scale 5, Chromatic Scale, Etude 1: Pitch, Etude 1: Rhythm, Etude 1:Articulation, Etude 1: Dynamics, Etude 1: Tone, Etude 1: Style, Etude 2: Pitch, Etude 2: Rhythm, Etude 2: Articulation, Etude 2: Dynamics, Etude 2: Tone, Etude 2: Style, Sight Read: Pitch, Sight Read: Rhythm\n"
-                        }
+                        csvText = "Varsity Concert Winds\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1, Scale 2, Scale 3, Scale 4, Scale 5, Chromatic Scale, Etude 1: Pitch, Etude 1: Rhythm, Etude 1:Articulation, Etude 1: Dynamics, Etude 1: Tone, Etude 1: Style, Etude 2: Pitch, Etude 2: Rhythm, Etude 2: Articulation, Etude 2: Dynamics, Etude 2: Tone, Etude 2: Style, Sight Read: Pitch, Sight Read: Rhythm\n"
+                        firstInstance = false
+                    }
                         
-                        let newLine = "\(rhythm.instrument), \(rhythm.last_name.replacingOccurrences(of: ",", with: ";")), \(rhythm.first_name.replacingOccurrences(of: ",", with: ";")), \(rhythm.finalScoreDouble), \(rhythm.comments.replacingOccurrences(of: ",", with: ";")), \(rhythm.scale1), \(rhythm.scale2), \(rhythm.scale3), \(rhythm.scale4), \(rhythm.scale5), \(rhythm.chromatic_scale), \(rhythm.etude1_pitch), \(rhythm.etude1_rhythm), \(rhythm.etude1_articulation), \(rhythm.etude1_dynamics), \(rhythm.etude1_tone), \(rhythm.etude1_style),  \(rhythm.etude2_pitch), \(rhythm.etude2_rhythm), \(rhythm.etude2_articulation), \(rhythm.etude2_dynamics), \(rhythm.etude2_tone), \(rhythm.etude2_style), \(rhythm.read_pitch), \(rhythm.read_rhythm)\n"
+                    let newLine = "\(rhythm.instrument), \(rhythm.last_name.replacingOccurrences(of: ",", with: ";")), \(rhythm.first_name.replacingOccurrences(of: ",", with: ";")), \(rhythm.finalScoreDouble), \(rhythm.comments.replacingOccurrences(of: ",", with: ";")), \(rhythm.scale1), \(rhythm.scale2), \(rhythm.scale3), \(rhythm.scale4), \(rhythm.scale5), \(rhythm.chromatic_scale), \(rhythm.etude1_pitch), \(rhythm.etude1_rhythm), \(rhythm.etude1_articulation), \(rhythm.etude1_dynamics), \(rhythm.etude1_tone), \(rhythm.etude1_style),  \(rhythm.etude2_pitch), \(rhythm.etude2_rhythm), \(rhythm.etude2_articulation), \(rhythm.etude2_dynamics), \(rhythm.etude2_tone), \(rhythm.etude2_style), \(rhythm.read_pitch), \(rhythm.read_rhythm)\n"
                         csvText.append(newLine)
                         
-                    }
-                }
-                for x in 0..<auditionsToConvert.count
-                {
-                    if let percussion = auditionsToConvert[x] as? varsityConcertPercussion
-                    {
-                        if x == 0
-                        {
-                            csvText.append("\nVarsity Concert Percussion\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1: Pitch, Scale 1: Production,  Scale 2: Pitch,  Scale 2: Production,  Scale 3: Pitch,  Scale 3: Production, Snare: Rhythm, Snare: Tempo, Snare: Dynamic, Snare: Production, Mallet: Rhythm, Mallet: Pitch, Mallet: Tempo, Mallet: Dynamic, Mallet: Production, Timpani: Rhythm, Timpani: Tempo,  Timpani: Dynamic,  Timpani: Production, Snare Reading: Rhythm, Snare Reading: Production, Mallet Reading: Rhythm, Mallet Reading: Pitch, Mallet Reading: Production\n")
-                        }
-                        
-                        let newLine = "\(percussion.instrument), \(percussion.last_name.replacingOccurrences(of: ",", with: ";")), \(percussion.first_name.replacingOccurrences(of: ",", with: ";")), \(percussion.finalScore), \(percussion.comments.replacingOccurrences(of: ",", with: ";")), \(percussion.scale1_pitch), \(percussion.scale1_production), \(percussion.scale2_pitch), \(percussion.scale2_production), \(percussion.scale3_pitch), \(percussion.scale3_production), \(percussion.snare_rhythm), \(percussion.snare_tempo), \(percussion.snare_dynamic), \(percussion.snare_production), \(percussion.mallet_rhythm), \(percussion.mallet_pitch),  \(percussion.mallet_tempo), \(percussion.mallet_dynamic), \(percussion.mallet_production), \(percussion.timpani_rhythm), \(percussion.timpani_tempo), \(percussion.timpani_dynamic), \(percussion.timpani_production), \(percussion.snareRead_rhythm), \(percussion.snareRead_production), \(percussion.malletRead_rhythm), \(percussion.malletRead_pitch), \(percussion.malletRead_production)\n"
-                        csvText.append(newLine)
-                    }
                 }
             }
-            do {
+            firstInstance = true
+            for x in 0..<auditionsToConvert.count
+            {
+                if let percussion = auditionsToConvert[x] as? varsityConcertPercussion
+                {
+                    if firstInstance
+                    {
+                        csvText.append("\nVarsity Concert Percussion\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1: Pitch, Scale 1: Production,  Scale 2: Pitch,  Scale 2: Production,  Scale 3: Pitch,  Scale 3: Production, Snare: Rhythm, Snare: Tempo, Snare: Dynamic, Snare: Production, Mallet: Rhythm, Mallet: Pitch, Mallet: Tempo, Mallet: Dynamic, Mallet: Production, Timpani: Rhythm, Timpani: Tempo,  Timpani: Dynamic,  Timpani: Production, Snare Reading: Rhythm, Snare Reading: Production, Mallet Reading: Rhythm, Mallet Reading: Pitch, Mallet Reading: Production\n")
+                        firstInstance = false
+                    }
+                        
+                    let newLine = "\(percussion.instrument), \(percussion.last_name.replacingOccurrences(of: ",", with: ";")), \(percussion.first_name.replacingOccurrences(of: ",", with: ";")), \(percussion.finalScore), \(percussion.comments.replacingOccurrences(of: ",", with: ";")), \(percussion.scale1_pitch), \(percussion.scale1_production), \(percussion.scale2_pitch), \(percussion.scale2_production), \(percussion.scale3_pitch), \(percussion.scale3_production), \(percussion.snare_rhythm), \(percussion.snare_tempo), \(percussion.snare_dynamic), \(percussion.snare_production), \(percussion.mallet_rhythm), \(percussion.mallet_pitch),  \(percussion.mallet_tempo), \(percussion.mallet_dynamic), \(percussion.mallet_production), \(percussion.timpani_rhythm), \(percussion.timpani_tempo), \(percussion.timpani_dynamic), \(percussion.timpani_production), \(percussion.snareRead_rhythm), \(percussion.snareRead_production), \(percussion.malletRead_rhythm), \(percussion.malletRead_pitch), \(percussion.malletRead_production)\n"
+                        csvText.append(newLine)
+                }
+            }
+            
+            do
+            {
                 try csvText.write(to: csvPath!, atomically: true, encoding: String.Encoding.utf8)
-            } catch {
+            }
+            catch
+            {
                 print("Failed to create Varsity Auditions CSV")
             }
             
@@ -70,13 +77,15 @@ class csvManagement: NSObject
                 }
             }
             
+            firstInstance = true
             for x in 0..<auditionsToConvert.count
             {
                 if let rhythm = auditionsToConvert[x] as? concertWinds
                 {
-                    if x == 0
+                    if firstInstance
                     {
                         csvText = "Freshmen Concert Winds\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1, Scale 2, Scale 3, Scale 4, Scale 5, Chromatic Scale, Etude 1: Pitch, Etude 1: Rhythm, Etude 1:Articulation, Etude 1: Dynamics, Etude 1: Tone, Etude 1: Style, Etude 2: Pitch, Etude 2: Rhythm, Etude 2: Articulation, Etude 2: Dynamics, Etude 2: Tone, Etude 2: Style, Sight Read: Pitch, Sight Read: Rhythm\n"
+                        firstInstance = false
                     }
                     
                     
@@ -85,13 +94,15 @@ class csvManagement: NSObject
                     
                 }
             }
+            firstInstance = true
             for x in 0..<auditionsToConvert.count
             {
                 if let percussion = auditionsToConvert[x] as? freshmenConcertPercussion
                 {
-                    if x == 0
+                    if firstInstance
                     {
                         csvText.append("\nFreshmen Concert Percussion\n, \nInstrument, Last Name, First Name, Final Score, Comments, Scale 1: Pitch, Scale 1: Production,  Scale 2: Pitch,  Scale 2: Production, Snare: Rhythm, Snare: Tempo, Snare: Dynamic, Snare: Production, Mallet: Rhythm, Mallet: Pitch, Mallet: Tempo, Mallet: Dynamic, Mallet: Production, Snare Reading: Rhythm, Snare Reading: Production, Mallet Reading: Rhythm, Mallet Reading: Pitch, Mallet Reading: Production\n")
+                        firstInstance = false
                     }
                     
                     
@@ -100,12 +111,15 @@ class csvManagement: NSObject
                 }
             }
             
-            do {
+            do
+            {
                 try csvText.write(to: csvPath!, atomically: true, encoding: String.Encoding.utf8)
-            } catch {
+            }
+            catch
+            {
                 print("Failed to create Freshmen Auditions CSV")
             }
-
+            
         case 2: //Jazz
             for x in 0..<jazzAuditions.count
             {
@@ -119,9 +133,10 @@ class csvManagement: NSObject
             {
                 if let rhythm = auditionsToConvert[x] as? jazzRhythms
                 {
-                    if x == 0
+                    if firstInstance
                     {
                         csvText = "Jazz Rhythms\n, \nInstrument, Preferred Range, Last Name, First Name, Final Score, Comments, Swing: Production, Swing: Rhythm, Swing: Articulations, Straight: Production, Straight: Rhythm, Straight: Articulations, Sight: Production, Sight: Rhythm, Sight: Articulations, Improvisation, Left Hand Indepenence\n"
+                        firstInstance = false
                     }
                     
                     
@@ -134,9 +149,10 @@ class csvManagement: NSObject
             {
                 if let drumset = auditionsToConvert[x] as? jazzDrumset
                 {
-                    if x == 0
+                    if firstInstance
                     {
                         csvText.append("\nJazz Drumset\n, \nInstrument, Last Name, First Name, Final Score, Comments, Swing: Support, Swing: Stability,  Swing: Articulations,  Straight: Support, Straight: Stability, Straight: Articulations, Sight: Support, Sight: Stability, Sight: Articulations, Improvisation, Left Hand Independence\n")
+                        firstInstance = false
                     }
                     
                     
